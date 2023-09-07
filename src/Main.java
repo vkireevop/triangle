74ee37f469928e65b4ec66980aa94af80dac25c3
vkireevop <94505101+vkireevop@users.noreply.github.com>
2023-09-07 17:45:07 +0300

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,93 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
-    public static void main(String[] args) {
-//        String a = "12";
-//        String b = "12";
-//        String c = "12";
-//       ArrayList<CoordinantForTriangle> result = CoordinantChecker(a,b,c);
-//        for (CoordinantForTriangle item : result) {
-//            System.out.println(item);
-//        }
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-    }
-    public static void triangleChecker (String a, String b, String c) {
-
-        // conver to float
-
-        // exptions
-
-        if (a == b && b==c )
-        {
-            System.out.println("Треугольник равностороний");
-        } else if ((a == b && c < a + b) || (b == c && a < a+b) || (a == c && b < a+c)) {
-            System.out.println("Треугольник равнобедренный");
-        } else if (c < a+b && a< a+c && b < a+c)
-        {
-            System.out.println( "Треугольник разностороний");
-        } else {
-            System.out.println(" Это не треугольник");
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("triangle_log.txt",true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(fileHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    public static ArrayList<CoordinantForTriangle> CoordinantChecker( String a, String b, String c)
-    {
-        triangleChecker(a,b,c);
-        CoordinantForTriangle A = new CoordinantForTriangle( 0, 0 );
-        CoordinantForTriangle B = new CoordinantForTriangle(a, 0);
-        float angleC = (float) Math.acos((a*a+b*b-c*c)/(2*a*b));
-        float xC = (float) (a * Math.cos(angleC));
-        float yC = (float) (a * Math.sin(angleC));
-        CoordinantForTriangle C = new CoordinantForTriangle(xC,yC);
-        ArrayList<CoordinantForTriangle> result = new ArrayList<>();
-        result.add(A);
-        result.add(B);
-        result.add(C);
-        return result;
+    public static void main(String[] args) {
+try {
+        String sideA = "1000";
+        String sideB = "1000";
+        String sideC = "1000";
+
+        List<CoordinantForTriangle> vertices = calculateVertices(sideA, sideB, sideC);
+        String triangleType = determineTriangleType(sideA,sideB,sideC);
+        if (!triangleType.equals(" ")) {
+            LOGGER.info("Тип треугольника: " + determineTriangleType(sideA, sideB, sideC));
+
+            LOGGER.info("Координаты вершин:");
+            for (int i = 0; i < 3; i++) {
+                LOGGER.info("Вершина " + (char)('А' + i) + ": (" + vertices.get(i).getX() + ", " + vertices.get(i).getY() + ")");
+            }
+            LOGGER.info("----------------------------------------");
+        } else {
+            LOGGER.info("Тип треугольника: Не треугольник");
+            LOGGER.info("Координаты вершин: (-1, -1), (-1, -1), (-1, -1)");
+            LOGGER.info("----------------------------------------");
+        }
+
+    } catch (NumberFormatException e) {
+        LOGGER.warning("Введены некорректные данные: " + e.getMessage());
+        LOGGER.info("Тип треугольника: ");
+        LOGGER.info("Координаты вершин: (-2, -2), (-2, -2), (-2, -2)");
+        LOGGER.info("----------------------------------------");
     }
 
 }
+
+
+    public static List<CoordinantForTriangle> calculateVertices (String a, String b, String c) {
+
+        float A = Float.parseFloat(a);
+        float B = Float.parseFloat(b);
+        float C = Float.parseFloat(c);
+        CoordinantForTriangle Ac = new CoordinantForTriangle( 0, 0 );
+        CoordinantForTriangle Bc = new CoordinantForTriangle(A, 0);
+        float angleC = (float) Math.acos((A * A + B * B - C * C)/(2 * A * B));
+        float xC = (float) (A * Math.cos(angleC));
+        float yC = (float) (A * Math.sin(angleC));
+        CoordinantForTriangle Cc = new CoordinantForTriangle(xC,yC);
+        double scaleFactor = 100.0 / Math.max(A, Math.max(B, C));
+        Ac.setLocation(Math.round(Ac.getX() * scaleFactor), Math.round(Ac.getY() * scaleFactor));
+        Bc.setLocation(Math.round(Bc.getX() * scaleFactor), Math.round(Bc.getY() * scaleFactor));
+        Cc.setLocation(Math.round(Cc.getX() * scaleFactor), Math.round(Cc.getY() * scaleFactor));
+        ArrayList<CoordinantForTriangle> result = new ArrayList<>();
+        result.add(Ac);
+        result.add(Bc);
+        result.add(Cc);
+        return result;
+    }
+    public static String determineTriangleType(String a, String b, String c) {
+
+        float A = Float.parseFloat(a);
+        float B = Float.parseFloat(b);
+        float C = Float.parseFloat(c);
+
+        if (A == B && B == C) {
+            return "Треугольник равносторонний";
+        } else if ((A == B && C < A + B) || (B == C && A < C + B) || (A == C && B < A + C)) {
+            return "Треугольник равнобедренный";
+        } else if (C < A + B && A < B + C && B < A + C) {
+            return "Треугольник разносторонний";
+        } else {
+            return " ";
+        }
+    }
+
+}
+
 class CoordinantForTriangle {
 
         float x;
@@ -60,4 +102,16 @@ class CoordinantForTriangle {
         public String toString(){
             return x + " " + y;
         }
+        public void setLocation (float x, float y) {
+            this.x = x;
+            this.y = y;
     }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+}
